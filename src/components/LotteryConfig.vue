531fcdbaa501a233b97d301b7bf8b0995f9f0cad
AlstonZZY <129561525+AlstonZZY@users.noreply.github.com>
2026-01-27 19:15:35 -0800

    Update LotteryConfig.vue

--- src/components/LotteryConfig.vue
+++ src/components/LotteryConfig.vue
@@ -31,7 +31,7 @@
             :step="1"
           ></el-input>
         </el-form-item>
-        <el-form-item label="八等獎：百搜禮卡一張">
+        <el-form-item label="十等獎：HMart $100現金禮券">
           <el-input
             type="number"
             v-model="form.firstPrize"
